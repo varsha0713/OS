4280f338151302aa4e5e2320624e14cc74e7ebbb
varsha0713 <99706376+varsha0713@users.noreply.github.com>
2023-07-18 10:25:42 +0000

    commit

--- bankers.c
+++ bankers.c
@@ -72,3 +72,28 @@ void main(){
     printf(">");
     // getch();
 }
+/* Enter the number of resources : 3
+enter the max instances of each resources
+a= 10
+b= 5
+c= 7
+
+ Enter the number of processes : 5
+
+ Enter the allocation matrix 
+      a b c
+P[0]  0 1 0
+P[1]  2 0 0 
+P[2]  3 0 2 
+P[3]  2 1 1
+P[4]  0 0 2
+
+Enter the max matrix 
+      a b c
+P[0]  7 5 3
+P[1]  3 2 2
+P[2]  9 0 2 
+P[3]  2 2 2
+P[4]  4 3 3
+
+         < P[1]  P[3]  P[4]  P[0]  P[2] > */
